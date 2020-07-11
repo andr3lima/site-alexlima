08df6d59239b23e2883ea7f6fcffe7720ea798ae
Anderson Aslap <anderson.adolfo1998@gmail.com>
2020-07-11 17:25:22 -0300

    [MASTER] - CONFIG ANEXO EMAIL

--- email/Email.php
+++ email/Email.php
@@ -7,9 +7,25 @@ class Email
     private $message;
     private $headers;
     private $anexo;
+    private $boundary;
 
     public function getMessage()
     {
+
+        $this->boundary = "XYZ-".md5(date("dmYis"))."-ZYX";
+
+        // Ou arquivo local
+        $path = '/caminho/para/o/arquivo';
+        $fileType = mime_content_type( $path );
+        $fileName = basename( $path );
+
+        // Pegando o conteúdo do arquivo
+        $fp = fopen( $path, "rb" ); // abre o arquivo enviado
+        $anexo = fread( $fp, filesize( $path ) ); // calcula o tamanho
+        $anexo = chunk_split(base64_encode( $anexo )); // codifica o anexo em base 64
+        fclose( $fp ); // fecha o arquivo
+
+        /*
         $this->message = '
                     <html>
                         <head>
@@ -21,7 +37,20 @@ class Email
                             <p>Aslap: '.$this->getAnexo().'</p>
                         </body>
                     </html>
-        ';
+        ';*/
+
+        $mensagem  = "--$this->boundary" . PHP_EOL;
+        $mensagem .= "Content-Type: text/html; charset='utf-8'" . PHP_EOL;
+        $mensagem .= "Mensagem: o e-book"; // Adicione aqui sua mensagem
+        $mensagem .= "--$this->boundary" . PHP_EOL;
+
+        $mensagem .= "Content-Type: ". $fileType ."; name=\"". $fileName . "\"" . PHP_EOL;
+        $mensagem .= "Content-Transfer-Encoding: base64" . PHP_EOL;
+        $mensagem .= "Content-Disposition: attachment; filename=\"". $fileName . "\"" . PHP_EOL;
+        $mensagem .= "$anexo" . PHP_EOL;
+        $mensagem .= "--$boundary" . PHP_EOL;
+        
+        $this->message = $mensagem;
         
         return $this->message;
     }
@@ -30,6 +59,9 @@ class Email
     {
         $this->headers  = 'MIME-Version: 1.0' . "\r\n";
         $this->headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+        $this->headers .= "Content-Type: multipart/mixed; ";
+        $this->headers .= "boundary=" . $this->boundary . PHP_EOL;
+        $this->headers .= "$this->boundary" . PHP_EOL;
 
         // Additional headers
         $this->headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
